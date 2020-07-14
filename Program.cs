a7da26626b7a9873319bd57fad0ac7ce10911703
Barry Griffith <barrygriffith@outlook.com>
2020-07-14 11:46:56 -0400

    Fixed my numbers conditional for chapter 4

--- Program.cs
+++ Program.cs
@@ -40,24 +40,29 @@ namespace lists_dictionaries
         //     rockyPlanets.ForEach(planet => Console.WriteLine($"Rocky planet: {planet}"));
 
 
-        Random random = new Random();
-List<int> numbers = new List<int> {
-    random.Next(6),
-    random.Next(6),
-    random.Next(6),
-    random.Next(6),
-    random.Next(6),
-    random.Next(6),
-};
+//         Random random = new Random();
+// List<int> numbers = new List<int> {
+//     random.Next(6),
+//     random.Next(6),
+//     random.Next(6),
+//     random.Next(6),
+//     random.Next(6),
+//     random.Next(6),
+// };
 
-for (int i=0; i<numbers.Count; i++) {
-   // Determine if the current loop index is contained inside of the `numbers` list. Print a message to the console indicating whether the index is in the list.
+// for (int i=0; i<numbers.Count; i++) {
+//    // Determine if the current loop index is contained inside of the `numbers` list. Print a message to the console indicating whether the index is in the list.
 
-   if(numbers.Contains(i)) {
-       Console.WriteLine($"{i} is in the numbers list");
-   }
+//    if(numbers.Contains(i)) {
+//        Console.WriteLine($"The numbers list does contain {i}");
+//    } else{
+//        Console.WriteLine($"Numbers does not contain {i}");
+//    };
    
-}
+// }
+
+
+
 
 
         }
